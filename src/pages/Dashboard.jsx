ba62f19f6aa5f5ea08a81559c854efa286c4027e
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-09 12:58:04 -0500

    added layout dashboard and home page files

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+        <h1>Dashboard Page</h1>
+    </>
   )
 }
 
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,10 @@
+
+const Layout = () => {
+  return (
+    <>
+        <h1>Layout Page</h1>
+    </>
+  )
+}
+
+export default Layout
