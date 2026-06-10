fa4a5c009f68b5cd277405b46e739e54a8c16361
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-10 11:27:31 -0500

    added features to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../components/Home/Banner"
 import Hero from "../components/Home/Hero"
+import Features from "../components/Home/Features"
 const Home = () => {
   return (
     <>
     <Banner /> 
     <Hero />
+    <Features /> 
     </>
   )
 }
