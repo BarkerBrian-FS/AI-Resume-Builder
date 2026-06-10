a95498419810adfe17292a1c6b2343bfd9e2e365
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-10 12:23:28 -0500

    added testimonial cards

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Title from "./Title";
 import { Zap } from 'lucide-react';
 
 const Features = () => {
@@ -10,7 +11,9 @@ const Features = () => {
             <Zap width={18}/>
             <span>Simple Process</span>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center">
+        <Title title="Build Your Resume" description="Our sreamlined process helps your create a professional resume 
+        in minutes with intelligent AI-Powered tools and features"/>
+        <div className="flex flex-col md:flex-row items-center justify-center xl:-mt-10">
             <img className="max-w-2xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
             <div className="px-4 md:px-0" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
                 <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Home/Banner"
 import Hero from "../components/Home/Hero"
 import Features from "../components/Home/Features"
+import Testimonials from "../components/Home/Testimonials"
 const Home = () => {
   return (
     <>
     <Banner /> 
     <Hero />
     <Features /> 
+    <Testimonials /> 
     </>
   )
 }
