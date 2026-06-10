d418600b4d9546f0defb219aedfab7b60c7a53da
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-10 12:02:29 -0500

    made title component

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { Zap } from 'lucide-react';
 
 const Features = () => {
     const [isHover, setIsHover] = React.useState(false);
 
   return (
-     <>
+     <div id="features" className="flex flex-col items-center my10 scroll-mt-12">
+     <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5">
+            <Zap width={18}/>
+            <span>Simple Process</span>
+        </div>
         <div className="flex flex-col md:flex-row items-center justify-center">
             <img className="max-w-2xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
             <div className="px-4 md:px-0" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
                 <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
-                    <div className={`p-6 group-hover:bg-violet-100 border border-transparent group-hover:border-violet-300  flex gap-4 rounded-xl transition-colors ${!isHover ? 'border-violet-300 bg-violet-100' : ''}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-6 stroke-violet-600"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" /><circle cx="16.5" cy="7.5" r=".5" fill="currentColor" /></svg>
+                    <div className={`p-6 group-hover:bg-green-100 border border-transparent group-hover:border-green-300  flex gap-4 rounded-xl transition-colors ${!isHover ? 'border-green-300 bg-green-100' : ''}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-6 stroke-green-600"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" /><circle cx="16.5" cy="7.5" r=".5" fill="currentColor" /></svg>
                         <div className="space-y-2">
                             <h3 className="text-base font-semibold text-slate-700">Real-Time Analytics</h3>
                             <p className="text-sm text-slate-600 max-w-xs">Get instant insights into your finances with live dashboards.</p>
@@ -27,8 +32,8 @@ const Features = () => {
                     </div>
                 </div>
                 <div className="flex items-center justify-center gap-6 max-w-md group cursor-pointer">
-                    <div className="p-6 group-hover:bg-orange-100 border border-transparent group-hover:border-orange-300 flex gap-4 rounded-xl transition-colors">
-                        <svg className="size-6 stroke-orange-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 15V3" /><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><path d="m7 10 5 5 5-5" /></svg>
+                    <div className="p-6 group-hover:bg-green-100 border border-transparent group-hover:border-green-300 flex gap-4 rounded-xl transition-colors">
+                        <svg className="size-6 stroke-green-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 15V3" /><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><path d="m7 10 5 5 5-5" /></svg>
                         <div className="space-y-2">
                             <h3 className="text-base font-semibold text-slate-700">Customizable Reports</h3>
                             <p className="text-sm text-slate-600 max-w-xs">Export professional, audit-ready financial reports for tax or internal review.</p>
@@ -44,7 +49,7 @@ const Features = () => {
                 font-family: 'Poppins', sans-serif;
             }
         `}</style>
-    </>
+    </div>
   )
 }
 
