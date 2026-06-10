4d1cda0f8945053838d64b7e5815034cada7fcd1
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-10 12:50:36 -0500

    added footer to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import Hero from "../components/Home/Hero"
 import Features from "../components/Home/Features"
 import Testimonials from "../components/Home/Testimonials"
 import CallToAction from "../components/Home/CallToAction"
-
+import Footer from "../components/Home/Footer"
 const Home = () => {
   return (
     <>
-    <Banner /> 
-    <Hero />
-    <Features /> 
-    <Testimonials /> 
-    <CallToAction />
+      <Banner /> 
+      <Hero />
+      <Features /> 
+      <Testimonials /> 
+      <CallToAction />
+      <Footer /> 
     </>
   )
 }
