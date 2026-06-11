37db9216d742a285ba87c1eaddeeb5c0042b386d
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-11 12:19:06 -0500

    added existing resume buttons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,19 @@
-import { PlusIcon, UploadCloudIcon } from "lucide-react"
+import { FilePenLine, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon } from "lucide-react"
+import { dummyResumeData } from "../assets/assets"
+import { useEffect, useState } from "react"
 
 const Dashboard = () => {
+
+  const colors = ['#9333ea', '#d97706', '#dc2626', '#0284c7', '#16a34a']
+  const [allResumes, setAllResumes] = useState([])
+
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData)
+  } 
+
+  useEffect(() => {
+    loadAllResumes()
+  },[])
   return (
     <>
     <div>
@@ -28,6 +41,27 @@ const Dashboard = () => {
             <p className="text-sm group-hover:text-purple-600 transition-all
             duration-300">Upload Resume</p>
           </button>
+        </div>
+        <hr className="border-slate-300 my-6 sm:w-76.25"></hr>
+        <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length]
+            return (
+              <button key={index} className="relative w-full sm:max-w-36 h-48 flex
+              flex-col items-center justify-center rounded-lg gap-2 border group hover-shadow-lg 
+              transition-all duration-300 cursor-pointer" style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)
+              `,borderColor: baseColor+'40'}}>
+              <FilePenLine className="size-7 group hover:scale-125 transition-all" style={{color: baseColor}}/> 
+              <p className="text-sm group hover:scale-125 transition-all px-2 text-center"style={{color: baseColor}}>{resume.title}</p>
+              <p className="absolute bottom-1 text-[11px] text-slate-400 group hover:text-slate-600 transition-all
+              duration-300 px-2  text-center"style={{color: baseColor+'90'}}>Updated On {new Date(resume.updatedAt).toLocaleDateString()}</p>
+              <div className="absolute top-1 right-1 group-hover:flex hidden items-center transition-all ">
+                <TrashIcon className="size-7 p-1.5 hover:bg-whie/50 rounded text-slate-700 transition-colors"/> 
+                <PencilIcon className="size-7 p-1.5 hover:bg-whie/50 rounded text-slate-700 transition-colors"/>
+              </div>
+              </button>
+            )
+          })}
         </div>
       </div>
     </div>
