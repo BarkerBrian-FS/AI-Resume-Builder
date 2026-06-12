857ec90be8cd6b383c03f6ac845ade12aee95262
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-12 10:50:50 -0500

    made pencil icon work to edit title

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -31,6 +31,10 @@ const Dashboard = () => {
     navigate(`/app/builder/res123`)
   }
 
+  const editTitle = async(event) => {
+    event.preventDefault()
+  }
+
   useEffect(() => {
     loadAllResumes()
   },[])
@@ -67,7 +71,7 @@ const Dashboard = () => {
           {allResumes.map((resume, index) => {
             const baseColor = colors[index % colors.length]
             return (
-              <button key={index} className="relative w-full sm:max-w-36 h-48 flex
+              <button key={index} onClick={()=>navigate(`/app/builder/${resume._id}`)} className="relative w-full sm:max-w-36 h-48 flex
               flex-col items-center justify-center rounded-lg gap-2 border group hover-shadow-lg 
               transition-all duration-300 cursor-pointer" style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)
               `,borderColor: baseColor+'40'}}>
@@ -77,7 +81,7 @@ const Dashboard = () => {
               duration-300 px-2  text-center"style={{color: baseColor+'90'}}>Updated On {new Date(resume.updatedAt).toLocaleDateString()}</p>
               <div className="absolute top-1 right-1 group-hover:flex hidden items-center transition-all ">
                 <TrashIcon className="size-7 p-1.5 hover:bg-whie/50 rounded text-slate-700 transition-colors"/> 
-                <PencilIcon className="size-7 p-1.5 hover:bg-whie/50 rounded text-slate-700 transition-colors"/>
+                <PencilIcon onClick={() => {setEditResumeId(resume._id); setTitle(resume.title)}} className="size-7 p-1.5 hover:bg-whie/50 rounded text-slate-700 transition-colors"/>
               </div>
               </button>
             )
@@ -125,6 +129,9 @@ const Dashboard = () => {
                       )}
                     </div>
                   </label>
+                  <input type="file" id="resume-input" accept=".pdf" hidden
+                    onChange={(e) => setResume(e.target.files[0])}
+                  />
                 </div>
                 <button className="w-full py-2 bg-green-600 text-white rounded
                 hover:bg-green-700 transition-colors">Upload Resume</button>
@@ -133,7 +140,25 @@ const Dashboard = () => {
                  setShowUploadResume(false); setTitle('') }}/> 
               </div>
             </form>
-          )}
+          )
+          }
+          
+          {editResumeId && (
+            <form onSubmit={editTitle} onClick={()=> setEditResumeId('')} className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50
+            z-10 flex items-center justify-center">
+              <div onClick={e => e.stopPropagation()} className="relative bg-slate-50 border shadow-md rounded-lg
+              w-full max-w-sm p-6">
+                <h2 className="text-xl font-bold mb-4">Edit Resume Title</h2>
+                <input onChange={(e)=> setTitle(e.target.value)} value={title} type="text" placeholder="Enter Resume Title" className="w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600" required/>
+                <button className="w-full py-2 bg-green-600 text-white rounded
+                hover:bg-green-700 transition-colors">Update</button>
+                <XIcon className="absolute top-4 right-4 text-slate-400 hover:text-slate-600
+                cursor-pointer transition-colors" onClick={()=> {
+                 setEditResumeId(''); setTitle('') }}/> 
+              </div>
+            </form>
+          )
+          }
         </div>
       </div>
     </div>
