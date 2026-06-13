b32bf04acb7faac1c9ad4c001b4b3911607c0ccf
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-13 10:25:51 -0500

    started resume preview put templates in components

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,9 +1,11 @@
 
-const ResumePreview = () => {
+const ResumePreview = ({data, template, accentColor, classes = ""}) => {
   return (
     <>
-    <div>
-        
+    <div className="w-full bg-gray-100">
+        <div id="resume-preview" className={"border border-gray-200 print:shadow-none print: border-none" + classes}>
+            
+        </div>
     </div>
     </>
   )
